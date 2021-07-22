ae89cca3528783b23c86d7656da93842271867fb
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-07-22 18:07:30 +0900

    add currency property

---
+++ src/section9/Dollar.java
@@ -0,0 +1,15 @@
+package section9;
+
+public class Dollar extends Money {
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	String currency() {
+		return "USD";
+	}
+
+	Money times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+}
---
+++ src/section9/Franc.java
@@ -0,0 +1,15 @@
+package section9;
+
+public class Franc extends Money {
+	Franc(int amount) {
+		this.amount = amount;
+	}
+
+	String currency() {
+		return "CHF";
+	}
+
+	Money times(int multiplier) {
+		return new Franc(amount * multiplier);
+	}
+}
