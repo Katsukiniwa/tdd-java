31a1436155d90ab16f1a91e3ca04cf6f59d24afd
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-07-22 18:00:12 +0900

    complete section8

--- src/section8/MoneyTest.java
+++ src/section8/MoneyTest.java
@@ -10,4 +10,20 @@ public class MoneyTest {
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
+
+	@Test
+	public void testEquality() {
+		assertEquals(Money.dollar(5), Money.dollar(5));
+		assertNotEquals(Money.dollar(5), Money.dollar(6));
+		assertEquals(Money.franc(5), Money.franc(5));
+		assertNotEquals(Money.franc(5), Money.franc(56));
+		assertNotEquals(Money.franc(5), Money.dollar(5));
+	}
+
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(Money.franc(10), five.times(2));
+		assertEquals(Money.franc(15), five.times(3));
+	}
 }
