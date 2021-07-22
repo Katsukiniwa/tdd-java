2cc23373a87d99f0df1c4ce7e735ec38d817644d
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-07-22 18:00:52 +0900

    fix

--- src/section8/MoneyTest.java
+++ src/section8/MoneyTest.java
@@ -16,7 +16,7 @@ public class MoneyTest {
 		assertEquals(Money.dollar(5), Money.dollar(5));
 		assertNotEquals(Money.dollar(5), Money.dollar(6));
 		assertEquals(Money.franc(5), Money.franc(5));
-		assertNotEquals(Money.franc(5), Money.franc(56));
+		assertNotEquals(Money.franc(5), Money.franc(6));
 		assertNotEquals(Money.franc(5), Money.dollar(5));
 	}
 
